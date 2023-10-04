13a7e63a8d6c9e8f41a98eb97204e0d8ddc9dc81
krllmm <kirilenkich@gmail.com>
2023-10-04 22:55:26 +0300

    login logic
    now only logged users(admins) can edit and create data

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -26,11 +26,13 @@
             <tr>
                 <td>
                     {{ $client->name }}
-                    <a href="{{ route('project.edit', $client->id) }}">
-                        <svg id="edit_icon" width="14" height="14" viewBox="0 0 24 24">
-                            <path d="M 19.171875 2 C 18.448125 2 17.724375 2.275625 17.171875 2.828125 L 16 4 L 20 8 L 21.171875 6.828125 C 22.275875 5.724125 22.275875 3.933125 21.171875 2.828125 C 20.619375 2.275625 19.895625 2 19.171875 2 z M 14.5 5.5 L 3 17 L 3 21 L 7 21 L 18.5 9.5 L 14.5 5.5 z"></path>
-                        </svg>
-                    </a>
+                    @if(Auth::check())
+                        <a href="{{ route('project.edit', $client->id) }}">
+                            <svg id="edit_icon" width="14" height="14" viewBox="0 0 24 24">
+                                <path d="M 19.171875 2 C 18.448125 2 17.724375 2.275625 17.171875 2.828125 L 16 4 L 20 8 L 21.171875 6.828125 C 22.275875 5.724125 22.275875 3.933125 21.171875 2.828125 C 20.619375 2.275625 19.895625 2 19.171875 2 z M 14.5 5.5 L 3 17 L 3 21 L 7 21 L 18.5 9.5 L 14.5 5.5 z"></path>
+                            </svg>
+                        </a>
+                    @endif
                 </td>
                 <td>{{ $client->company }}</td>
                 <td>{{ $client->address }}</td>
@@ -42,8 +44,11 @@
 
 </div>
 
-<div class="button_container">
-    <a class="button-61" href="{{ route('client.create') }}">New client</a>
-</div>
+
+@if(Auth::check())
+    <div class="button_container">
+        <a class="button-61" href="{{ route('client.create') }}">New client</a>
+    </div>
+@endif
 
 @endsection
--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -25,11 +25,13 @@
             <tr>
                 <td>
                     {{ $task->title }}
-                    <a href="{{ route('task.edit', $task->id) }}">
-                        <svg id="edit_icon" width="14" height="14" viewBox="0 0 24 24">
-                            <path d="M 19.171875 2 C 18.448125 2 17.724375 2.275625 17.171875 2.828125 L 16 4 L 20 8 L 21.171875 6.828125 C 22.275875 5.724125 22.275875 3.933125 21.171875 2.828125 C 20.619375 2.275625 19.895625 2 19.171875 2 z M 14.5 5.5 L 3 17 L 3 21 L 7 21 L 18.5 9.5 L 14.5 5.5 z"></path>
-                        </svg>
-                    </a>
+                    @if(Auth::check())
+                        <a href="{{ route('task.edit', $task->id) }}">
+                            <svg id="edit_icon" width="14" height="14" viewBox="0 0 24 24">
+                                <path d="M 19.171875 2 C 18.448125 2 17.724375 2.275625 17.171875 2.828125 L 16 4 L 20 8 L 21.171875 6.828125 C 22.275875 5.724125 22.275875 3.933125 21.171875 2.828125 C 20.619375 2.275625 19.895625 2 19.171875 2 z M 14.5 5.5 L 3 17 L 3 21 L 7 21 L 18.5 9.5 L 14.5 5.5 z"></path>
+                            </svg>
+                        </a>
+                    @endif
                 </td>
                 <td>{{ $task->description }}</td>
                 <td>{{ $task->created_at->format('Y-m-d') }}</td>
@@ -41,10 +43,11 @@
 
 </div>
 
-<div class="button_container">
-    <a class="button-61" href="{{ route('task.create') }}">New task</a>
-</div>
-
+@if(Auth::check())
+    <div class="button_container">
+        <a class="button-61" href="{{ route('task.create') }}">New task</a>
+    </div>
+@endif
 
 
 
